b86c3b339515e8d0cf7c161bfefbd2b6922e18ee
TakoyakiDayo <Takoyaki1051@outlook.jp>
2025-02-10 21:32:31 +0900

    サインアップページとホームページの実装、不要なファイルの削除

--- src/components/sigin-form.tsx
+++ src/components/sigin-form.tsx
@@ -1,10 +1,10 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Card, CardContent } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import Image from "next/image"
-export function LoginForm({
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Image from 'next/image';
+export function SiginupForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
@@ -12,12 +12,22 @@ export function LoginForm({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden">
         <CardContent className="grid p-0 md:grid-cols-2">
+          <div className="relative hidden bg-muted md:block">
+            <Image
+              src="https://ui.shadcn.com/placeholder.svg"
+              alt="Image"
+              layout="fill"
+              objectFit="cover"
+              className="dark:brightness-[0.2] dark:grayscale"
+            />
+          </div>
+
           <form className="p-6 md:p-8">
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
-                <h1 className="text-2xl font-bold">Welcome back</h1>
+                <h1 className="text-2xl font-bold">Create an account</h1>
                 <p className="text-balance text-muted-foreground">
-                  Login to your Takoyaki&apos;s Accounts
+                  Sigin to your Takoyaki&apos;s Accounts
                 </p>
               </div>
               <div className="grid gap-2">
@@ -42,26 +52,18 @@ export function LoginForm({
                 <Input id="password" type="password" required />
               </div>
               <Button type="submit" className="w-full">
-                Login
+                signup
               </Button>
-              
+
               <div className="text-center text-sm">
                 Don&apos;t have an account?{" "}
-                <a href="signup" className="underline underline-offset-4">
-                  Sign up
+                <a href="login" className="underline underline-offset-4">
+                  Login
                 </a>
               </div>
             </div>
           </form>
-          <div className="relative hidden bg-muted md:block">
-            <Image
-              src="https://ui.shadcn.com/placeholder.svg"
-              alt="Image"
-              layout="fill"
-              objectFit="cover"
-              className="absolute inset-0 dark:brightness-[0.2] dark:grayscale"
-            />
-          </div>
+          
         </CardContent>
       </Card>
       {/*<div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">*/}
@@ -69,5 +71,5 @@ export function LoginForm({
       {/*  and <a href="#">Privacy Policy</a>.*/}
       {/*</div>*/}
     </div>
-  )
+  );
 }
