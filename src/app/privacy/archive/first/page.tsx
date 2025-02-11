5f789bce1eb488be4ff94a97788f580961b437ed
TakoyakiDayo <Takoyaki1051@outlook.jp>
2025-02-11 20:56:32 +0900

    プライバシーポリシーページの内容を更新し、データセキュリティ、画像管理、サービスの可用性に関するセクションを追加。ログインフォームにプライバシーポリシーへのリンクを追加。

--- src/app/privacy/archive/first/page.tsx
+++ src/app/privacy/archive/first/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
 				<Card>
 					<CardHeader>
 						<CardTitle>Privacy Policy</CardTitle>
-						<CardDescription>11 Feb 2025</CardDescription>
+						<CardDescription>Last Modified: 11 Feb 2025</CardDescription>
 						<CardDescription>
 							Previous Version:{" "}
 							<Link href="/privacy/archive/first">
@@ -23,10 +23,31 @@ export default function Page() {
 						</CardDescription>
 					</CardHeader>
 					<CardContent>
-						<p>Card Content</p>
+						<h3>Data Security</h3>
+						<p>
+							We are not responsible for any data breaches involving personal
+							information, including but not limited to unauthorized access,
+							hacking, or unintended data exposure.
+						</p>
+						<h3>mage Management</h3>
+						<p>
+							We are not responsible for any leakage of images uploaded by
+							users, whether due to system vulnerabilities, third-party
+							interference, or accidental disclosure. Additionally, we are not
+							responsible for any loss of images uploaded by users, whether
+							caused by system malfunctions, maintenance activities, or
+							unforeseen technical issues.
+						</p>
+						<h3>Service Availability</h3>
+						<p>
+							As this service is managed by a student, it may be discontinued at
+							any time due to financial, academic, or personal reasons. We bear
+							no responsibility for such termination or any resulting
+							inconvenience to users.
+						</p>
 					</CardContent>
 					<CardFooter>
-						<p>Card Footer</p>
+						<p>Written by Takoyaki</p>
 					</CardFooter>
 				</Card>
 			</Card>
