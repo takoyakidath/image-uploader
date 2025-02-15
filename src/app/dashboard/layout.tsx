71e61337c3c4ef4bded2806f18bc36e50c1952d2
TakoyakiDayo <Takoyaki1051@outlook.jp>
2025-02-15 20:29:53 +0900

    feat(dashboard): simplify layout and enhance sidebar navigation
    Refactored the dashboard layout by removing unnecessary components such as the header and sidebar from the main layout file. The sidebar is now integrated directly into the page component, providing a cleaner structure. Additionally, new breadcrumb components have been introduced to improve navigation hierarchy and user experience. This change streamlines the layout and makes it more intuitive for users to navigate through different sections of the application.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,3 @@
-import HeaderDath from "@/components/dashheader";
-import { AppSidebar } from "@/components/sidebar";
 type Props = {
 	children: React.ReactNode;
 };
@@ -7,14 +5,7 @@ type Props = {
 const MainLayout = ({ children }: Props) => {
 	return (
 		<>
-			<div className="bg-slate-500 ">
-				<HeaderDath />
-			</div>
-
-			<AppSidebar />
-			<main className="pl-[256px] h-full">
-				<div className="h-full bg-white">{children}</div>
-			</main>
+			<main>{children}</main>
 		</>
 	);
 };
