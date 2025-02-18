13e033180098cc6b5036f02b4c85336e4e8b5ea1
TakoyakiDayo <Takoyaki1051@outlook.jp>
2025-02-18 21:10:35 +0900

    refactor(dashboard): streamline layout and remove unnecessary components
    Reorganized the dashboard layout by removing redundant sidebar and breadcrumb components from individual pages, centralizing them in the MainLayout component. This change simplifies the component structure, enhances readability, and promotes code reuse. The updates contribute to a more consistent user interface across the dashboard while reducing the overall complexity of the page files.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,46 +1,9 @@
-import { AppSidebar } from "@/components/sidebar";
-import { ModeToggle } from "@/components/theme-toggle";
-import {
-	Breadcrumb,
-	BreadcrumbItem,
-	BreadcrumbLink,
-	BreadcrumbList,
-} from "@/components/ui/breadcrumb";
-import { Separator } from "@/components/ui/separator";
-import {
-	SidebarInset,
-	SidebarProvider,
-	SidebarTrigger,
-} from "@/components/ui/sidebar";
 export default function Page() {
 	return (
-		<SidebarProvider>
-			<AppSidebar />
-			<SidebarInset>
-				<header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 justify-between">
-					<div className="flex items-center gap-2">
-						<SidebarTrigger className="-ml-1" />
-						<Separator orientation="vertical" className="mr-2 h-4" />
-						<Breadcrumb>
-							<BreadcrumbList>
-								<BreadcrumbItem className="hidden md:block">
-									<BreadcrumbLink href="/dashboard">dashboard</BreadcrumbLink>
-								</BreadcrumbItem>
-							</BreadcrumbList>
-						</Breadcrumb>
-					</div>
-					<div className="flex items-center gap-2">
-						<ModeToggle />
-					</div>
-				</header>
-				<div className="flex flex-1 flex-col gap-4 p-4">
-					<div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
-						Home <br />
-						total size <br />
-						total Images <br />
-					</div>
-				</div>
-			</SidebarInset>
-		</SidebarProvider>
+		<div>
+			Home <br />
+			total size <br />
+			total Images <br />
+		</div>
 	);
 }
