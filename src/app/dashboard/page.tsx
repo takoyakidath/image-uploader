dec0715a14a765298246651c2aa0317bb76bfdc9
TakoyakiDayo <Takoyaki1051@outlook.jp>
2025-02-19 17:28:51 +0900

    feat(dashboard): add statistics and upload link to gallery
    This commit introduces a new section to the dashboard that displays key statistics, including total images and size metrics. Additionally, it enhances the gallery component by providing a direct link to an upload page, allowing users to easily add new content. This improves user experience by offering quick access to essential information and functions within the dashboard context.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,7 +6,20 @@ export default function Page() {
 			<div className="flex flex-1 flex-col gap-4 p-4">
 				<Search />
 				<div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
+					<div className="grid auto-rows-min gap-4 md:grid-cols-3">
+						<div className="aspect-video rounded-sm bg-muted/50">
+							<div className="text-6xl">total Images : 900k</div>
+						</div>
+						<div className="aspect-video rounded-sm bg-muted/50">
+							<div className="text-6xl">total size : 1PB</div>
+						</div>
+						<div className="aspect-video rounded-sm bg-muted/50">a</div>
+					</div>
+					<div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+				</div>
+				<div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
 					<div className="flex flex-1 flex-col gap-4 p-4">
+						<div className="text-6xl">Recently uploaded</div>
 						<Gallerys />
 						<div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
 					</div>
--- src/components/gallerys.tsx
+++ src/components/gallerys.tsx
@@ -1,9 +1,15 @@
+import { Plus } from "lucide-react";
+import Link from "next/link";
 export default function gallerys() {
 	return (
 		<>
 			<div className="flex flex-1 flex-col gap-4 p-4 overflow-auto">
 				<div className="grid auto-rows-min gap-4 md:grid-cols-3">
-					<div className="aspect-video rounded-xl bg-muted/50" />
+					<Link href="/dashboard/upload/">
+						<div className="aspect-video rounded-xl bg-muted/50">
+							<Plus className="size-full" />
+						</div>
+					</Link>
 					<div className="aspect-video rounded-xl bg-muted/50" />
 					<div className="aspect-video rounded-xl bg-muted/50" />
 					<div className="aspect-video rounded-xl bg-muted/50" />
