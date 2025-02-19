ba80b735956b54f3cda10984f4c8da4e80e34e0b
TakoyakiDayo <Takoyaki1051@outlook.jp>
2025-02-19 15:45:42 +0900

    feat(dashboard): enhance page layout and integrate search component
    This commit modifies the dashboard page layout to include a structured arrangement with a search component and a gallery section. The new design utilizes flexbox for better responsiveness and user experience. Additionally, the search component is updated to manage focus states, displaying suggestions only when the input field is focused. These changes aim to improve usability and aesthetic appeal on the dashboard interface.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,17 @@
+import Gallerys from "@/components/gallerys";
+import Search from "@/components/search";
 export default function Page() {
 	return (
 		<div>
-			Home <br />
-			total size <br />
-			total Images <br />
+			<div className="flex flex-1 flex-col gap-4 p-4">
+				<Search />
+				<div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
+					<div className="flex flex-1 flex-col gap-4 p-4">
+						<Gallerys />
+						<div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
 	Command,
 	CommandEmpty,
@@ -7,24 +8,33 @@ import {
 	CommandList,
 	CommandSeparator,
 } from "@/components/ui/command";
+import { useState } from "react";
 
 export default function Search() {
+	const [isFocused, setIsFocused] = useState(false);
+
 	return (
-		<>
-			<Command>
-				<CommandInput placeholder="Type a command or search..." />
-				<CommandList>
-					<CommandEmpty>No results found.</CommandEmpty>
-					<CommandGroup heading="Suggestions">
-						<CommandItem>from:</CommandItem>
-					</CommandGroup>
-					<CommandSeparator />
-					<CommandGroup heading="name">
-						<CommandItem>id</CommandItem>
-						<CommandItem>name</CommandItem>
-					</CommandGroup>
-				</CommandList>
-			</Command>
-		</>
+		<Command>
+			<CommandInput
+				placeholder="Type a command or search..."
+				onFocus={() => setIsFocused(true)}
+				onBlur={() => setIsFocused(false)}
+			/>
+			<CommandList>
+				{isFocused && (
+					<>
+						<CommandEmpty>No results found.</CommandEmpty>
+						<CommandGroup heading="Suggestions">
+							<CommandItem>from:</CommandItem>
+						</CommandGroup>
+						<CommandSeparator />
+						<CommandGroup heading="name">
+							<CommandItem>id</CommandItem>
+							<CommandItem>name</CommandItem>
+						</CommandGroup>
+					</>
+				)}
+			</CommandList>
+		</Command>
 	);
 }
