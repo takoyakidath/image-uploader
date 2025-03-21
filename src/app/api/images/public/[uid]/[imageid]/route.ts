33d20e29e3f7b9282268efb1f47c7d7a42e38980
TakoyakiDayo <Takoyaki1051@outlook.jp>
2025-03-21 18:00:42 +0900

    デプロイできるようにした　これからはdevブランチで開発していく予定

--- src/app/api/images/public/[uid]/[imageid]/route.ts
+++
@@ -1,9 +0,0 @@
-import { type NextRequest, NextResponse } from "next/server";
-
-export async function GET(
-	req: NextRequest,
-	{ params }: { params: { uid: string; imageid: string } },
-) {
-	const { uid, imageid } = await params;
-	return NextResponse.json({ message: `UID: ${uid}, ImageID: ${imageid}` });
-}
--- src/app/api/manage/private/remove/[uid]/[imageid]/route.ts
+++
@@ -1,9 +0,0 @@
-import { type NextRequest, NextResponse } from "next/server";
-
-export async function DELETE(
-	req: NextRequest,
-	{ params }: { params: { uid: string; imageid: string } },
-) {
-	const { uid, imageid } = params;
-	return NextResponse.json({ message: `UID: ${uid}, ImageID: ${imageid}` });
-}
